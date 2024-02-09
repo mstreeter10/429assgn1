b2772cb5b2aff57512d4d4851527a0be6063661c
Austin Barr <abarr2118@gmail.com>
2024-02-09 16:53:45 -0500

    changed batch file to compile all folders into classes

--- runTestAssgn1.bat
+++ runTestAssgn1.bat
@@ -1,3 +1,3 @@
-javac -d "classes" -classpath "classes;." model\*.java
-javac -classpath "classes;." TestAssgn1.java
-java -cp "mariadb-java-client-3.0.3.jar;classes;." TestAssgn1
+javac -d classes -classpath classes;. model\*.java common\*.java database\*.java event\*.java exception\*.java impresario\*.java Utilities\*.java
+javac -classpath classes;. TestAssgn1.java
+java -cp mariadb-java-client-3.0.3.jar;classes;. TestAssgn1
